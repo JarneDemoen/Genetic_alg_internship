a9b58cd84d6520d09ef594e21e9b81d18145f86f
JarneDemoen <jarne.demoen@student.howest.be>
2023-04-13 16:57:16 -0300

    Experimenting with fitness function

--- code/main.py
+++ code/main.py
@@ -7,8 +7,8 @@ import os
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 # Inputs user
-semester = "even"
-timeslots_day = [
+semester = "uneven"
+timeslots_per_day = [
     "19:00-19:50",
     "19:50-20:40",
     "20:55-21:45",
@@ -24,7 +24,7 @@ dataset_classesPP = pd.read_csv('../data/ClassesPP.csv', sep=';')
 dataset_professors = pd.read_csv('../data/Professors.csv', sep=';')
 dataset_courseSchedule_semester_uneven = dataset_courseSchedule[dataset_courseSchedule['ET'] % 2 != 0]
 dataset_courseSchedule_semester_even = dataset_courseSchedule[dataset_courseSchedule['ET'] % 2 == 0]
-timeslots = [timeslots_day for i in range(len(days))]
+timeslots = [timeslots_per_day for i in range(len(days))]
 class_types = ["AT", "AP", "AV"]
 
 genome_size_even = 0
@@ -47,12 +47,12 @@ elif semester == "uneven":
     dataset_courseSchedule_semester = dataset_courseSchedule_semester_uneven
     genome_size = genome_size_uneven
 
-professor_courses = {}
+professor_classes = {}
 for professor in dataset_professors['CODE']:
-    professor_courses[professor] = []
+    professor_classes[professor] = []
     for index in range(len(dataset_classesPP)):
         if dataset_classesPP['PROFESSOR CODE'][index] == professor:
-            professor_courses[professor].append(dataset_classesPP['DISCIPLINA'][index])
+            professor_classes[professor].append(dataset_classesPP['DISCIPLINA'][index])
 
 # functions
 def generate_bit_length(db_length):
@@ -114,7 +114,7 @@ def translate_genome(genome, hex_= False, string_= False):
             class_groups_translation = class_groups[class_group_index]
             professor_translation = dataset_professors['CODE'][professor_index]
             timeslot_day_translation = days[timeslot_day_index]
-            timeslot_translation = timeslots_day[timeslot_index]
+            timeslot_translation = timeslots_per_day[timeslot_index]
 
             translation_string.append({'class': class_translation, 'class_type': class_type_translation, 'class_group': class_groups_translation, 'professor': professor_translation, 'timeslot_day': timeslot_day_translation, 'timeslot': timeslot_translation, 'et': dataset_courseSchedule_semester['ET'][class_index]})
             
@@ -133,7 +133,7 @@ len_class_types_encoding = generate_bit_length(len(class_types))
 len_class_groups_encoding = generate_bit_length(len(class_groups))
 len_professors_encoding = generate_bit_length(len(dataset_professors))
 len_timeslots_day_encoding = generate_bit_length(len(days))
-len_timeslots_encoding = generate_bit_length(len(timeslots_day))
+len_timeslots_encoding = generate_bit_length(len(timeslots_per_day))
 
 def generate_binary(length, db):
     binary_value = 100000
@@ -154,7 +154,7 @@ def create_genome_part():
     binary_class_group = generate_binary(len_class_groups_encoding, class_groups)
     binary_professor = generate_binary(len_professors_encoding, dataset_professors)
     binary_timeslot_day = generate_binary(len_timeslots_day_encoding, days)
-    binary_timeslot = generate_binary(len_timeslots_encoding, timeslots_day)
+    binary_timeslot = generate_binary(len_timeslots_encoding, timeslots_per_day)
     genome = binary_class + binary_class_type + binary_class_group + binary_professor + binary_timeslot_day + binary_timeslot
     return genome
 
@@ -171,49 +171,29 @@ def generate_population(population_size):
         population.append(generate_genome(genome_size))
     return population
 
-def check_100_rule(genome):
-    violations = 0
-    for day in days:
-        classes_day = [class_scheduling for class_scheduling in genome if class_scheduling['timeslot_day'] == day]
-        if len(classes_day) == 0:
-            continue
-        for index_timeslot in range(len(timeslots_day) - 1):
-            for index_class_day in range(len(classes_day) - 1):
-                # check if there is a class scheduled on the first timeslot of the day
-                if classes_day[index_class_day]['timeslot'] == timeslots_day[0]:
-                    # check if there is a class scheduled on the next timeslot, if not, there is a violation
-                    if classes_day[index_class_day + 1] != timeslots_day[1]:
-                        violations += 1
-                        print(f"Class {classes_day[index_class_day]} is scheduled at the first timeslot of the day and there is no class scheduled on the next timeslot")
-                
-                # check if there is a class scheduled on the last timeslot of the day
-                if classes_day[index_class_day]['timeslot'] == timeslots_day[-1]:
-                    # check if there is a class scheduled on the previous timeslot, if not, there is a violation
-                    if classes_day[index_class_day - 1] != timeslots_day[-2]:
-                        violations += 1
-                        print(f"Class {classes_day[index_class_day]} is scheduled at the last timeslot of the day and there is no class scheduled on the previous timeslot")
-                
-                if index_class_day != 0:
-                    if classes_day[index_class_day - 1] != timeslots_day[index_timeslot - 1] and classes_day[index_class_day + 1] != timeslots_day[index_timeslot + 1]:
-                        violations += 1   
-                        print("There is no class scheduled at the previous and next timeslot of class ", classes_day[index_class_day])
 
-        
+def get_violation_count_assigning_professor(genome):
+    print("Professor classes")
+    print(professor_classes)
+    print("------------------")
+    violations = 0
+    for class_scheduling in genome:
+        professor = class_scheduling['professor']
+        class_name = class_scheduling['class'][:5]
+        if class_name not in professor_classes[professor]:
+            violations += 1
+            print(f"The professor {professor} is not competent of teaching {class_name}")
 
     return violations
             
 
 def calculate_fitness_score(genome):
     translated_genome = translate_genome(genome, string_=True)
-    violations_100_rule = check_100_rule(translated_genome)
-    print("Violations 100 rule: ", violations_100_rule)
+    violations_assingning_professor = get_violation_count_assigning_professor(translated_genome)
 
 def run_genetic_algorithm(generation_limit, fitness_limit):
     # population = generate_population(20)
     genome = generate_genome(genome_size)
-    translation = translate_genome(genome, string_=True)
     fitness_score = calculate_fitness_score(genome)
-    print_per_line(translation)
-    print(dataset_courseSchedule_semester)
 
 run_genetic_algorithm(generation_limit=100, fitness_limit=25)
